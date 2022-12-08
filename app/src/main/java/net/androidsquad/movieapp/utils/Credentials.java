865005de73c00c6fbc96c10ac16f033d3d96544d
2022172Samnang <2022172pheng@aupp.edu.kh>
2022-12-08 18:56:24 +0700

    Initial commit

---
+++ app/src/main/java/net/androidsquad/movieapp/utils/Credentials.java
@@ -0,0 +1,9 @@
+package net.androidsquad.movieapp.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    public static final String API_KEY = "811ad0754682e997bf8e407fb67101cf";
+    public static boolean POPULAR = false;
+}
